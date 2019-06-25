3d31310e72e35c5a5288772543859033d98abe51
frank-carracedo <fjmc81@gmail.com>
2019-06-25 17:48:59 +0100

    step15-pending

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,19 +1,32 @@
 require "bike"
 
 class DockingStation
-  
-  attr_reader :docker  
+
+  attr_reader :docker
 
   def initialize()
     @docker = []
+    @capacity = 20
   end
 
   def release_bike
-    raise "something no bikes literally"
-    #return Bike.new
+    if @docker.empty?
+      raise "something no bikes literally"
+    else
+      @docker.pop()
+    end
   end
+
+  def full?
+    @docker.length >= 20
+  end
+
   def dock bike
-    @docker.push(bike)
+    if !full?
+      @docker.push(bike)
+    else
+      raise "docking station is full"
+    end
   end
   def docker_empty?
     @docker.empty?
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,16 +10,22 @@ describe DockingStation do
     expect(docker_station).to be_instance_of(DockingStation)
   end
   it "#release_bike" do
-
-    #expect(docker_station.release_bike).to be_instance_of(Bike)
+    docker_station.docker.push(Bike.new)
+    expect(docker_station.release_bike).to be_instance_of(Bike)
     expect{ docker_station.release_bike }.to raise_error
   end
-  it "" do
+  it "dock" do
     bike = Bike.new
     expect(docker_station).to respond_to(:dock)
+    #expect{docker_station.dock(bike)}.to raise_error
   end
   it "docker is empty?" do
-    
+
     expect(docker_station.docker_empty?).to be true
+  end
+  it "docker capacity is 20?" do
+    docker2_station = DockingStation.new
+    20.times { docker2_station.dock Bike.new}
+    expect(docker2_station.docker.length).to eq(20)
   end
 end
